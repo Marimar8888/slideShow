da7144f6c69703c52a31ad094dc6f72ceebd5efc
Marimar <malonso@quadram.mobi>
2024-07-26 11:51:21 +0200

    slide carousel finisth

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/scss/image-gallery.scss";
+
+
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1023/1000/600/",
+    thumbnail: "https://picsum.photos/id/1023/250/150/",
+  },
+];
+
 
 export default class App extends Component {
+
   render() {
+  
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-      </div>
-    );
-  }
+      <ImageGallery
+      items={images}
+      showPlayButton = {false}
+      showThumbnails = {false}
+      showFullscreenButton= {false}
+      autoPlay = {true}
+      renderItem={(item) => (
+        <div style={{ width: '80vw', height: '300px', margin: 'auto' }}>
+          <img
+            src={item.original}
+            alt={item.originalAlt}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+        </div>
+      )}
+       />
+    )}
 }
+
